e5ec1d5432c0eac300f195dcdf390d7ad66e1e9b
Hendrik du Toit <hendrik@brightedge.co.za>
2022-05-05 02:40:17 +0200

    modified:   venvnew.bat

--- venvnew.bat
+++ venvnew.bat
@@ -11,14 +11,13 @@ if "%2"=="" (
     ) else (
     set _python_version=%2
 )
-@REM SET %PATH%=C:\Python\Python%_python_version%;C:\Python\Python%_python_version%\Scripts;%PATH%
+
 call deactivate
 "c:\Python\Python%_python_version%\python" -m venv --clear d:\venv\%_project_name%_env
 call d:\venv\%_project_name%_env\Scripts\activate.bat
+
 python.exe -m pip install --upgrade pip
-@REM IF EXIST "d:\dropbox\batch\venv_all_venvs_req.txt" (
-@REM 	pip install -r d:\dropbox\batch\venv_all_venvs_req.txt
-@REM )
+
 IF EXIST "d:\Dropbox\Projects\%_project_name%" (
 	cd d:\Dropbox\Projects\%_project_name%
 	pip install -r d:\Dropbox\Projects\%_project_name%\requirements.txt
@@ -28,18 +27,6 @@ IF EXIST "d:\Dropbox\Projects\%_project_name%" (
 	md d:\Dropbox\Projects\%_project_name%
 	cd d:\Dropbox\Projects\%_project_name%
 )
-@REM IF EXIST "d:\dropbox\batch\venv_%_project_name%_req.txt" (
-@REM 	pip install --upgrade -r d:\dropbox\batch\venv_%_project_name%_req.txt
-@REM 	) ELSE (
-@REM 	TYPE NUL > "d:\dropbox\batch\venv_%_project_name%_req.txt"
-@REM )
-
-@REM IF EXIST "d:\dropbox\batch\venv_%_project_name%_req.bat" (
-@REM 	call d:\dropbox\batch\venv_%_project_name%_req.bat %_project_name%
-@REM 	) ELSE (
-@REM 	ECHO @ECHO ON > "d:\dropbox\batch\venv_%_project_name%_req.bat"
-@REM 	ECHO pip install -e . >> "d:\dropbox\batch\venv_%_project_name%_req.bat"
-@REM )
 
 IF EXIST "d:\dropbox\batch\venv_%_project_name%_setup.bat" (
 	call d:\dropbox\batch\venv_%_project_name%_setup.bat %_project_name%
