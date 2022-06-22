a5f400bcf9e318186c27ae74f30a1b67736e8654
Hendrik du Toit <hendrik@brightedge.co.za>
2022-06-22 23:33:37 +0200

    modified:   venv_batch_setup.bat

--- venv_batch_setup.bat
+++ venv_batch_setup.bat
@@ -3,11 +3,4 @@ set _python_base_dir="c:\Program Files Python"
 set _venv_base_dir=d:\venv
 set _batch_dir=d:\dropbox\batch
 set _projects_dir=d:\Dropbox\Projects
-@REM W5195935
-if "%COMPUTERNAME%"=="W5202690" (
-	set _python_base_dir=c:\Python
-	set _venv_base_dir=c:\venv
-	set _batch_dir=d:\batch
-	set _projects_dir=d:\Projects
-)
 cd %_batch_dir%
