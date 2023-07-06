8ce2b1b698c057b3e39ccde36d4ccd04623c5588
DirkdToit <dirk@brightedge.co.za>
2023-07-06 16:38:10 +0200

    Updated new_rn with new directories

--- new_rn.bat
+++ new_rn.bat
@@ -17,9 +17,9 @@ cd /d "%projects_base_dir%"
 
 REM Check if the project name is provided as the first argument
 if "%~1"=="" (
-    set /p project_name=What will the name of the project be? 
+    set /p "project_name=What will the name of the project be? "
 ) else (
-    set project_name=%~1
+    set "project_name=%~1"
 )
 
 REM Check if the project directory already exists
@@ -33,9 +33,11 @@ if "%~2"=="" (
     :prompt_add_plop
     set /p "add_plop=Please state whether you would like to add plop templates? (y/n): "
     if /i "%add_plop%"=="y" (
+        set "add_plop=y"
         goto :continue
     ) else (
         if /i "%add_plop%"=="n" (
+            set "add_plop=n"
             goto :continue
         ) else (
             echo Invalid input. Please enter "y" or "n".
@@ -43,7 +45,7 @@ if "%~2"=="" (
         )
     )
 ) else (
-    :prompt_add_plop
+    :validate_add_plop
     if /i "%~2%"=="y" (
         set "add_plop=y"
     ) else (
@@ -51,7 +53,7 @@ if "%~2"=="" (
             set "add_plop=n"
         ) else (
             echo Invalid input. Please enter "y" or "n".
-            goto :prompt_add_plop
+            exit /b
         )
     )
 )
@@ -61,68 +63,40 @@ if "%~2"=="" (
 REM Check if the third argument is provided and validate it as "Expo" or "PRN"
 if "%~3"=="" (
     :prompt_project_type
-    echo project promt
     set /p "project_type=Which type of project do you want? (Expo/Pure React Native (PRN)): "
     if /i "%project_type%"=="Expo" (
+        set "project_type=Expo"
         goto :continue_project_type
     ) else (
         if /i "%project_type%"=="PRN" (
+            set "project_type=PRN"
             goto :continue_project_type
         ) else (
             echo Invalid input. Please enter "Expo" or "PRN".
-            goto :prompt_project_type
+            exit /b
         )
     )
 ) else (
-    :prompt_project_type
+    :validate_project_type
     if /i "%~3%"=="Expo" (
         set "project_type=Expo"
-        goto :continue_project_type
     ) else (
         if /i "%~3%"=="PRN" (
             set "project_type=PRN"
         ) else (
             echo Invalid input. Please enter "Expo" or "PRN".
+            exit /b
         )
     )
 )
 
 :continue_project_type
 
-REM Check the project type and perform the appropriate version check
-if /i "%project_type%"=="Expo" (
-    REM Check if expo-cli is installed and get the version
-    for /f "tokens=*" %%I in ('npm show expo-cli version --silent') do set "latest_version=%%I"
-
-    REM Define the desired version
-    for /f "delims=" %%A in ('expo --version') do set "current_version=%%A"
-
-    REM Compare the desired version with the latest version
-    if "%current_version%" neq "%latest_version%" (
-        echo Installing the latest version of expo-cli...
-        call npm install -g expo-cli@latest
-    ) else (
-        echo expo-cli is already up to date
-    )
-) else (
-    REM Check if react-native-cli is installed and get the version
-    for /f "tokens=*" %%I in ('npm show react-native version --silent') do set "latest_version=%%I"
-
-    REM Define the desired version
-    for /f "delims=" %%A in ('react-native --version') do set "current_version=%%A"
-
-    REM Compare the desired version with the latest version
-    if "%current_version%" neq "%latest_version%" (
-        echo Installing the latest version of react-native-cli...
-        call npm install -g react-native-cli@latest
-    ) else (
-        echo react-native-cli is already up to date
-    )
-)
+call npx install expo
 
 REM Run "expo init" or "react-native init" with the provided project name and select the appropriate template option based on the project type
 if /i "%project_type%"=="Expo" (
-    call npx expo-cli init "%project_name%" --template blank
+    call npx create-expo-app "%project_name%" --template blank
 ) else (
     call npx react-native init "%project_name%"
 )
@@ -133,9 +107,12 @@ REM Create the "src" directory in the newly created project
 mkdir "src"
 
 REM Create additional directories inside the "src" directory
-mkdir "src\Components"
-mkdir "src\Screens"
-mkdir "src\Styles"
+mkdir "src\components"
+mkdir "src\screens"
+mkdir "src\styles"
+mkdir "src\api"
+mkdir "src\hooks"
+mkdir "src\context"
 
 REM If the user input is "y", copy the plop templates
 if /i "%add_plop%"=="y" (
