01b16ec6c6996891008183859765527ac2c55f43
Hendrik du Toit <hendrik@brightedge.co.za>
2023-08-31 14:51:33 +0200

    Changed venvnew.bat

--- env_var_loc_dev.bat
+++ env_var_loc_dev.bat
@@ -1,18 +1,18 @@
 @ECHO Initialize local development environment variables
-IF /I "%1"=="ON" (
+IF /I "%2"=="ON" (
     set _debug=ON
     ) ELSE (
     set _debug=OFF
 )
 ECHO %_debug%
-SET MYSQL_TCP_PORT_RTE=50001
-SET MYSQL_TCP_PORT_EXAMPLES=50002
-SET MYSQL_TCP_PORT_TRANS=60001
-REM SET VENV_PYTHON_BASE=c:\Python
-REM SET VENV_BASE=d:\venv
-IF /I "%1"=="ON" (
-    set _debug=ON
-    ) ELSE (
-    set _debug=OFF
+if %1=="" (
+    SET MYSQL_TCP_PORT=3006
+) else if %1==rte-db (
+    SET MYSQL_TCP_PORT=50001
+) else if %1==SQLAlchemyExample (
+    SET MYSQL_TCP_PORT=50002
+) else if %1==FastAPIExample (
+    SET MYSQL_TCP_PORT=50002
+) else if %1==rte-api (
+    SET MYSQL_TCP_PORT=50003
 )
-
