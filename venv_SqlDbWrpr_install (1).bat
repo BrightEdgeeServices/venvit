e16696a99e3ff30dfb0f20941ad282c0d25426de
Hendrik du Toit <hendrik@brightedge.co.za>
2023-10-07 12:53:49 +0200

    Delete files not to be synched

--- venv_SqlDbWrpr_install (1).bat
+++
@@ -1,9 +0,0 @@
-@ECHO %2 
-@ECHO Running venv_SqlDbWrpr_install.bat...
-git init
-pip install --upgrade --force black
-pip install --upgrade --force flake8
-pip install --upgrade --force pre-commit
-pre-commit install
-pre-commit autoupdate
-IF EXIST "D:\Dropbox\Projects\BEE\SqlDbWrpr\pyproject.toml" pip install -e .
--- venv_rte-installbackendserver_install (1).bat
+++
@@ -1,9 +0,0 @@
-@ECHO %2 
-@ECHO Running venv_rte-installbackendserver_install.bat...
-git init
-pip install --upgrade --force black
-pip install --upgrade --force flake8
-pip install --upgrade --force pre-commit
-pre-commit install
-pre-commit autoupdate
-IF EXIST "d:\Dropbox\Projects\RTE\rte-installbackendserver\pyproject.toml" pip install -e .
